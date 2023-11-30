bc662544532c86721dd526d9fd44d2e56c28bb76
Rentlli <youssefhussenmohamed@gmail.com>
2023-11-30 17:13:44 +0200

    customeradmin

---
+++ src/main/java/com/example/carrental/Admin.java
@@ -0,0 +1,19 @@
+package com.example.carrental;
+
+public class Admin extends User{
+
+
+    static int Aid=0;
+    final int Admin_ID;
+
+    public Admin(String first_Name,String last_Name,String email_Adress,int phone_Number){
+        super(first_Name,last_Name,email_Adress,phone_Number);
+        Admin_ID=Aid;
+        Aid++;
+
+    }
+
+    public int getAdmin_ID() {
+        return Admin_ID;
+    }
+}
