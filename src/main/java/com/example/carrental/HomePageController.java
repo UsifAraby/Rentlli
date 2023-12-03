eeac60cbbea885eff800038be3c65cbb01b28850
Rentlli <youssefhussenmohamed@gmail.com>
2023-12-03 13:03:35 +0200

    init commit

--- src/main/java/com/example/carrental/HomePageController.java
+++ src/main/java/com/example/carrental/HomePageController.java
@@ -1,17 +1,39 @@
 package com.example.carrental;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 
-public class RectangleAnimation {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.time.format.TextStyle;
+import java.util.ResourceBundle;
 
-
-
+public class HomePageController implements Initializable {
+    @FXML
+    public AnchorPane rootPane;
     @FXML
     private Rectangle rectangle1;
-
     @FXML
     private Rectangle rectangle2;
     @FXML
@@ -27,7 +49,9 @@ public class RectangleAnimation {
 
 
 
-    public void initialize() {
+
+    public void Hover() {
+
         setupHoverEffect(rectangle1,car1);
         setupHoverEffect(rectangle2,car2);
         setupHoverEffect(rectangle3,car3);
@@ -69,6 +93,27 @@ public class RectangleAnimation {
 
 
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       rootPane.setOpacity(0);
+        makeFadeInTransition();
+        Hover();
+    }
+
+
+    public void makeFadeInTransition() {
+
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setNode(rootPane);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+
+    }
+
+
 }
 
 
