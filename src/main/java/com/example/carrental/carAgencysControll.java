d87ce49c3b695f7c3b81174882492a80f07cbba0
Mohamed <muhammedhany510@gmail.com>
2023-12-07 15:24:36 +0200

    init commit

--- src/main/java/com/example/carrental/carAgencysControll.java
+++ src/main/java/com/example/carrental/carAgencysControll.java
@@ -1,14 +1,5 @@
 package com.example.carrental;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.shape.Rectangle;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,26 +7,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.time.format.TextStyle;
 import java.util.ResourceBundle;
 
-public class HomePageController implements Initializable {
-
+public class carAgencysControll  implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private Label carAgencyName;
     @FXML
     public AnchorPane rootPane;
     @FXML
@@ -51,11 +40,12 @@ public class HomePageController implements Initializable {
     @FXML
     private Rectangle rectangle6;
     @FXML
-    ImageView car1,car2,car3,car4,car5,car6;
+    ImageView car1,car2,car3,car4,car5,car6,ELKERSH,ELLITHY,ELGOHARY,ELMASRIA;
     @FXML
-    private javafx.scene.control.ChoiceBox<String> ChoiceBox;
+    private Label myLabel;
+
     @FXML
-    private Label carAgencyName;
+    private ChoiceBox<String> ChoiceBox;
     @FXML
     private String[] carAgencys = {"ELKERSH","ELLAITHY","ELMASRYA","ELGOHARY"};
 
@@ -66,9 +56,13 @@ public class HomePageController implements Initializable {
         setupHoverEffect(rectangle1,car1);
         setupHoverEffect(rectangle2,car2);
         setupHoverEffect(rectangle3,car3);
-        setupHoverEffect(rectangle4,car4);
-        setupHoverEffect(rectangle5,car5);
-        setupHoverEffect(rectangle6,car6);
+        //setupHoverEffect(rectangle4,ELKERSH);
+        //setupHoverEffect(rectangle4,ELGOHARY);
+        //setupHoverEffect(rectangle4,ELLITHY);
+        //setupHoverEffect(rectangle4,ELMASRIA);
+        // setupHoverEffect(rectangle4,car4);
+        //setupHoverEffect(rectangle5,car5);
+        //setupHoverEffect(rectangle6,car6);
 
     }
 
@@ -105,66 +99,61 @@ public class HomePageController implements Initializable {
 
     }
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       rootPane.setOpacity(0);
+        rootPane.setOpacity(0);
         makeFadeInTransition();
         Hover();
         ChoiceBox.getItems().addAll(carAgencys);
-       ChoiceBox.setOnAction(event -> {
+        ChoiceBox.setOnAction(event -> {
             try {
-                makefadeout();
-              getcarAgency(event);
-
-
+                getcarAgency(event);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
-
         });
     }
     public void getcarAgency(ActionEvent event) throws IOException {
-        String myCarAgency=ChoiceBox.getValue();
-        System.out.println(myCarAgency);
-     if(myCarAgency.equals(carAgencys[0])){
+        String myCarAgency = ChoiceBox.getValue();
 
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency1.fxml"));
-          root = loader.load();
-         stage = (Stage) rootPane.getScene().getWindow();
-         scene = new Scene(root);
-         stage.setScene(scene);
-         stage.show();
-
-       } else if (myCarAgency.equals(carAgencys[1])) {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency2.fxml"));
-         root = loader.load();
-         stage = (Stage) rootPane.getScene().getWindow();
-         scene = new Scene(root);
-         stage.setScene(scene);
-         stage.show();
-     }
-     else if (myCarAgency.equals(carAgencys[2])) {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency3.fxml"));
-         root = loader.load();
-         stage = (Stage) rootPane.getScene().getWindow();
-         scene = new Scene(root);
-         stage.setScene(scene);
-         stage.show();
-     }
-     else if (myCarAgency.equals(carAgencys[3])) {
-         FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency4.fxml"));
-         root = loader.load();
-         stage = (Stage) rootPane.getScene().getWindow();
-         scene = new Scene(root);
-         stage.setScene(scene);
-         stage.show();
-     }
+       if (myCarAgency.equals(carAgencys[0])) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency1.fxml"));
+            root = loader.load();
+            stage = (Stage) rootPane.getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+       else if (myCarAgency.equals(carAgencys[1])) {
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency2.fxml"));
+           root = loader.load();
+           stage = (Stage) rootPane.getScene().getWindow();
+           scene = new Scene(root);
+           stage.setScene(scene);
+           stage.show();
+       }
+       else if (myCarAgency.equals(carAgencys[2])) {
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency3.fxml"));
+           root = loader.load();
+           stage = (Stage) rootPane.getScene().getWindow();
+           scene = new Scene(root);
+           stage.setScene(scene);
+           stage.show();
+       }
+       else if (myCarAgency.equals(carAgencys[3])) {
+           FXMLLoader loader = new FXMLLoader(getClass().getResource("carAgency4.fxml"));
+           root = loader.load();
+           stage = (Stage) rootPane.getScene().getWindow();
+           scene = new Scene(root);
+           stage.setScene(scene);
+           stage.show();
+       }
     }
     public void makefadeout(){
         FadeTransition fadeTransition = new FadeTransition();
-        fadeTransition.setDuration(Duration.millis(2000));
+        fadeTransition.setDuration(Duration.millis(3000));
         fadeTransition.setNode(rootPane);
         fadeTransition.setFromValue(1);
         fadeTransition.setToValue(0);
@@ -176,7 +165,7 @@ public class HomePageController implements Initializable {
     public void makeFadeInTransition() {
 
         FadeTransition fadeTransition = new FadeTransition();
-        fadeTransition.setDuration(Duration.millis(1000));
+        fadeTransition.setDuration(Duration.millis(2500));
         fadeTransition.setNode(rootPane);
         fadeTransition.setFromValue(0);
         fadeTransition.setToValue(1);
@@ -185,11 +174,12 @@ public class HomePageController implements Initializable {
     }
 
 
+    }
 
 
 
 
-}
+
 
 
 
